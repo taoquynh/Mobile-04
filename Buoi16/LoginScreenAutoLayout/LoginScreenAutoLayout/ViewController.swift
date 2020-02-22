be7594469f2e5c16b377712f21abb9e545975a70
taoquynh <taothuyquynh.ks@gmail.com>
2020-02-22 14:39:12 +0700

    update

---
+++ Buoi16/LoginScreenAutoLayout/LoginScreenAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginScreenAutoLayout
+//
+//  Created by Quynh on 2/22/20.
+//  Copyright © 2020 Quynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
